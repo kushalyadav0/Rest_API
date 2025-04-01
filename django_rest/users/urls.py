a6987be02279dddaa0d1942e79330026098d7435
kushalyadav0 <net.kushalyadav@gmail.com>
2025-04-01 23:20:10 +0530

    removed authentication

--- django_rest/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-from .views import SignUp
-urlpatterns = [
-    path('Login/', views.LogIn, name='LogIn'),
-    path('Signup/', views.SignUp, name='SignUp')
-    # path('signup/', SignUp.as_view(), name = 'Signup'),   
-]
