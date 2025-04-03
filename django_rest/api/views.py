987a26215ed8b9b9844a5718e47613357cff960c
kushalyadav0 <net.kushalyadav@gmail.com>
2025-04-04 00:23:28 +0530

    ViewSets completed but here as we need to use one class but we need to write lots of lines of code so from now on we will be using modelViewSet

--- django_rest/api/views.py
+++ django_rest/api/views.py
@@ -157,5 +157,21 @@ class companiesViewset(viewsets.ViewSet): # extending viewset class
             return Response(serializer.data, status= status.HTTP_201_CREATED)
         return Response(serializer.errors)
     
-    def update(self, request, pk):
-        company = get_object_or_404(Companies)
+    def retrieve(self, request, pk= None):
+        companies = get_object_or_404(Companies, pk= pk)
+        serializer = companiesSerializer(companies)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def update(self, request, pk= None):
+        company = Companies.objects.get(pk = pk)
+        serializer = companiesSerializer(company    , data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk=None):
+        company = get_object_or_404(Companies, pk= pk)
+        company.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
