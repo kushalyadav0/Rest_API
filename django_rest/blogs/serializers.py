6fbf1216571c8bbe9386d2eeb965f7d6fd5e528f
kushalyadav0 <net.kushalyadav@gmail.com>
2025-04-05 22:54:20 +0530

    included comments under their respective blogs using basename that we've used in foreign key in comments model to link blogs model and comments model

--- django_rest/blogs/serializers.py
+++ django_rest/blogs/serializers.py
@@ -1,13 +1,15 @@
 from rest_framework import serializers
 from .models import Blog, Comments
 
-class BlogSerializer(serializers.ModelSerializer):
-    class Meta():
-        model = Blog
-        fields ='__all__'
-
 
 class CommentsSerializer(serializers.ModelSerializer):
     class Meta():
         model = Comments
+        fields ='__all__' 
+        
+class BlogSerializer(serializers.ModelSerializer):
+    comments = CommentsSerializer(many = True, read_only = True)
+    class Meta():
+        model = Blog
         fields ='__all__'
+
